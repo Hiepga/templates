bce9fa2523fd2adbbdb9152264a6e04e6373d704
Hiepga <chanhhiep.0201@gmail.com>
2016-11-17 07:52:06 +0700

    up

---
+++ huehomehotel/js/main.js
@@ -0,0 +1,20 @@
+ jQuery(document).ready(function ($) {
+    var options = {
+        $FillMode: 2,                                       
+        $AutoPlay: true,
+        $Idle: 4000,  
+    };
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+    function ScaleSlider() {
+        var bodyWidth = document.body.clientWidth;
+        if (bodyWidth)
+            jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+    ScaleSlider();
+
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+});
